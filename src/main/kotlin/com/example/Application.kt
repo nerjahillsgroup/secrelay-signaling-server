dd0bf329cc63e16b7aedb8df23330f71acf635b8
joannaro <erozekdk@gmail.com>
2025-08-11 04:37:33 +0200

    Fix missing imports in Application.kt

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,5 +1,8 @@
 package com.example
 
+import com.example.plugins.configureRouting
+import com.example.plugins.configureSerialization
+import com.example.plugins.configureSockets
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
